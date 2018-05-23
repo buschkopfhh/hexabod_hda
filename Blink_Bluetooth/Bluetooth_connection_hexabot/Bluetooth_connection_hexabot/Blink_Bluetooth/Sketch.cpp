476291966ae251b974fb90be8bfcd5c65a822ab1
Henri Hildebrand <henri.hildebrand@gmail.com>
2018-05-23 16:44:58 +0200

    added first classes

--- Blink_Bluetooth/Bluetooth_connection_hexabot/Bluetooth_connection_hexabot/Blink_Bluetooth/Sketch.cpp
+++ Blink_Bluetooth/Bluetooth_connection_hexabot/Bluetooth_connection_hexabot/Blink_Bluetooth/Sketch.cpp
@@ -1,8 +1,11 @@
 ﻿/*Begining of Auto generated code by Atmel studio */
 #include <Arduino.h>
 #include <HardwareSerial.h>
+#include <move.h>
+#include <string.h>
 
 /*End of auto generated code by Atmel studio */
+
 
 char data = 0;
 // the setup function runs once when you press reset or power the board
@@ -18,15 +21,8 @@ void setup() {
 	Serial.println(" ");
 	Serial.println("To connect, start the LED Controller App on your Smartphone");
 	Serial.println("and connect to Bluetooth HC-05");
-
 	
 	digitalWrite(LED_BUILTIN, HIGH); 
-
-	
-	
-	
-
-
   
 }
 
@@ -45,18 +41,27 @@ if(Serial.available() > 0){ // Checks whether data is comming from the serial po
 	digitalWrite(12, HIGH);
 	Serial.println("LED is ON");
 	
+	move to; // class move, object: to
+	
 	}
+
+  else if(data == '0')       //Checks whether value of data is equal to 0
+    {
+	digitalWrite(12, LOW);   //If value is 0 then LED turns OFF
+ //   digitalWrite(LED_BUILTIN, LOW);
+ 	Serial.println("LED is OFF");
+	 
+	
+	 
+	 
+	 
+	 
+	}
+	}
+}
 	/*for(i=0; i<10; i++) {
 		digitalWrite(13, HIGH);  //If value is 1 then LED turns ON
 		delay(50);
 		digitalWrite(13, LOW);
 		delay(50);
 	}*/
-  else if(data == '0')       //Checks whether value of data is equal to 0
-    {
-	digitalWrite(12, LOW);   //If value is 0 then LED turns OFF
- //   digitalWrite(LED_BUILTIN, LOW);
- 	Serial.println("LED is OFF");
-	}
-	}
-}
